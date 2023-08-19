c2509aeb04c9ad2c4c9d012546282547dfedc180
SteveLRojas <looserr2@illinois.edu>
2023-08-19 16:30:43 +0200

    Fixed tabs, made command parsing more robust

--- CH376S/CH376S.ino
+++ CH376S/CH376S.ino
@@ -4,125 +4,112 @@
 
 void usb_autoconfig()
 {
-  Serial.println("USB Autoconfig:");
-  usb_check_exist();
-  usb_get_version();
-  usb_set_mode_5();
-  usb_set_mode_7();
-  usb_set_mode_6();
-  usb_disk_connect();
-  usb_disk_mount();
-  usb_get_status();
-  usb_disk_query();
+	Serial.println("USB Autoconfig:");
+	usb_check_exist();
+	usb_get_version();
+	usb_set_mode_5();
+	usb_set_mode_7();
+	usb_set_mode_6();
+	usb_disk_connect();
+	usb_disk_mount();
+	usb_get_status();
+	usb_disk_query();
 }
 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  SPI.begin();
-  SPI.setClockDivider(2);
-  SPI.setDataMode(SPI_MODE0);
-  pinMode(10, OUTPUT);
-  
-  for(int d = 0; d<256; ++d)
-  {
-    read_buff[d] = NULL;
-  }
-  
-  usb_reset_all();
-  delay(100);
-  Serial.print("\n");
-  usb_autoconfig();
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	SPI.begin();
+	SPI.setClockDivider(2);
+	SPI.setDataMode(SPI_MODE0);
+	pinMode(10, OUTPUT);
+
+	for(int d = 0; d<256; ++d)
+	{
+		read_buff[d] = NULL;
+	}
+
+	usb_reset_all();
+	delay(100);
+	Serial.print("\n");
+	usb_autoconfig();
 }
 
 void loop()
 {
-  //test
-  char hex[3];
-  uint16_t test_word;
-  test_word = get_hex();
-  byte_to_hex(test_word, hex);
-  Serial.println(hex);
-  delay(1000);
-  return;
-  
-  // put your main code here, to run repeatedly:
-  if(Serial.available())
-  {
-    switch(Serial.parseInt())
-    {
-      case 0:
-        usb_check_exist();
-        break;
-      case 1:
-        usb_get_version();
-        break;
-      case 2:
-        usb_set_mode_5();
-        break;
-      case 3:
-        usb_set_mode_7();
-        break;
-      case 4:
-        usb_set_mode_6();
-        break;
-      case 5:
-        usb_disk_connect();
-        break;
-      case 6:
-        usb_disk_mount();
-        break;
-      case 7:
-        usb_get_status();
-        break;
-      case 8:
-        usb_set_file_name();
-        break;
-      case 9:
-        usb_file_open();
-        break;
-      case 10:
-        usb_get_file_size();
-        break;
-      case 11:
-        usb_file_close();
-        break;
-      case 12:
-        usb_byte_read();
-        break;
-      case 13:
-        usb_byte_read_go();
-        break;
-      case 14:
-        usb_read_data0();
-        break;
-      case 15:
-        usb_autoconfig();
-        break;
-      case 16:
-        usb_reset_all();
-        break;
-      case 17:
-        usb_file_read();
-        break;
-      case 18:
-        usb_disk_capacity();
-        break;
-      case 19:
-        usb_disk_query();
-        break;
-      case 20:
-        print_test_data();
-        break;
-      case 21:
-        usb_file_create();
-        break;
-      case 22:
-        usb_write_test_data();
-        break;
-    }
-  delay(100);
-  Serial.println("Ready.");
-  }
+	switch(get_hex())
+	{
+		case 0x00:
+			usb_check_exist();
+			break;
+		case 0x01:
+			usb_get_version();
+			break;
+		case 0x02:
+			usb_set_mode_5();
+			break;
+		case 0x03:
+			usb_set_mode_7();
+			break;
+		case 0x04:
+			usb_set_mode_6();
+			break;
+		case 0x05:
+			usb_disk_connect();
+			break;
+		case 0x06:
+			usb_disk_mount();
+			break;
+		case 0x07:
+			usb_get_status();
+			break;
+		case 0x08:
+			usb_set_file_name();
+			break;
+		case 0x09:
+			usb_file_open();
+			break;
+		case 0x0A:
+			usb_get_file_size();
+			break;
+		case 0x0B:
+			usb_file_close();
+			break;
+		case 0x0C:
+			usb_byte_read();
+			break;
+		case 0x0D:
+			usb_byte_read_go();
+			break;
+		case 0x0E:
+			usb_read_data0();
+			break;
+		case 0x0F:
+			usb_autoconfig();
+			break;
+		case 0x10:
+			usb_reset_all();
+			break;
+		case 0x11:
+			usb_file_read();
+			break;
+		case 0x12:
+			usb_disk_capacity();
+			break;
+		case 0x13:
+			usb_disk_query();
+			break;
+		case 0x14:
+			print_test_data();
+			break;
+		case 0x15:
+			usb_file_create();
+			break;
+		case 0x16:
+			usb_write_test_data();
+			break;
+	}
+	delay(100);
+	Serial.println("Ready.");
 }
--- CH376S/support.h
+++ CH376S/support.h
@@ -1,25 +1,25 @@
 typedef union Bitfield_16
 {
-  struct Bits_16
-  {
-    unsigned bit0 : 1;
-    unsigned bit1 : 1;
-    unsigned bit2 : 1;
-    unsigned bit3 : 1;
-    unsigned bit4 : 1;
-    unsigned bit5 : 1;
-    unsigned bit6 : 1;
-    unsigned bit7 : 1; 
-    unsigned bit8 : 1;
-    unsigned bit9 : 1;
-    unsigned bit10 : 1;
-    unsigned bit11 : 1;
-    unsigned bit12 : 1;
-    unsigned bit13 : 1;
-    unsigned bit14 : 1;
-    unsigned bit15 : 1; 
-  } bits;
-  unsigned int value;
+	struct Bits_16
+	{
+		unsigned bit0 : 1;
+		unsigned bit1 : 1;
+		unsigned bit2 : 1;
+		unsigned bit3 : 1;
+		unsigned bit4 : 1;
+		unsigned bit5 : 1;
+		unsigned bit6 : 1;
+		unsigned bit7 : 1; 
+		unsigned bit8 : 1;
+		unsigned bit9 : 1;
+		unsigned bit10 : 1;
+		unsigned bit11 : 1;
+		unsigned bit12 : 1;
+		unsigned bit13 : 1;
+		unsigned bit14 : 1;
+		unsigned bit15 : 1; 
+	} bits;
+	unsigned int value;
 } bitfield_16_t;
 
 extern bitfield_16_t bitfield_16;
